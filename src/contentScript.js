7327e9a91931ae9be3d37fc0f3f33071ccfb9b80
Praveen <praveennooka123@gmail.com>
2024-07-30 20:45:30 +0530

    fixed a minor bug

--- src/contentScript.js
+++ src/contentScript.js
@@ -3,28 +3,31 @@ function addCustomTab(listId) {
 
   if (tabContainer) {
     const customTab = document.createElement('li');
-    customTab.innerHTML = `<a href="https://codeforces.com/contest/1995/standings?list=${listId}" id="custom-standings-tab">College Standings</a>`;
-
-    const friendsStandingsTab = tabContainer.querySelector('a[href*="standings/friends"]');
-    friendsStandingsTab.parentNode.insertAdjacentElement('afterend', customTab);
-
-    const tabs = tabContainer.querySelectorAll('li');
-    customTab.addEventListener('click', (event) => {
-      // Remove the selectedLava class from all tabs
-      tabs.forEach(tab => {
-        tab.classList.remove('current', 'selectedLava');
-      });
-      // Set the clicked tab as selected
-      customTab.classList.add('current', 'selectedLava');
-    });
-
-    // Check the current URL to see if we should pre-select this tab
     const currentUrl = window.location.href;
-    if (currentUrl.includes(`standings?list=${listId}`)) {
-      tabs.forEach(tab => {
-        tab.classList.remove('current', 'selectedLava');
+    const contestIdMatch = currentUrl.match(/\/contest\/(\d+)/);
+
+    if (contestIdMatch) {
+      const contestId = contestIdMatch[1];
+      const standingsUrl = `https://codeforces.com/contest/${contestId}/standings?list=${listId}`;
+      customTab.innerHTML = `<a href="${standingsUrl}" id="custom-standings-tab">College Standings</a>`;
+
+      const friendsStandingsTab = tabContainer.querySelector('a[href*="standings/friends"]');
+      friendsStandingsTab.parentNode.insertAdjacentElement('afterend', customTab);
+
+      const tabs = tabContainer.querySelectorAll('li');
+      customTab.addEventListener('click', (event) => {
+        tabs.forEach(tab => {
+          tab.classList.remove('current', 'selectedLava');
+        });
+        customTab.classList.add('current', 'selectedLava');
       });
-      customTab.classList.add('current', 'selectedLava');
+
+      if (currentUrl.includes(`standings?list=${listId}`)) {
+        tabs.forEach(tab => {
+          tab.classList.remove('current', 'selectedLava');
+        });
+        customTab.classList.add('current', 'selectedLava');
+      }
     }
   }
 }
